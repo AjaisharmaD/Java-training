9c0ebe6f2ac0ba9b6bf3fae6bfd1efdce8d73d69
Ajaisharma <ajaisharma187@gmail.com>
2022-08-09 09:12:23 +0530

    Add files via upload

---
+++ 08-08-2022/slice.java
@@ -0,0 +1,14 @@
+/*
+ * A simple java class which is executing the Slicing concept in an  * Array to reverse the char array's last 5 elements
+ * @Ajaisharma 
+ */
+class Slice {
+    public static void main(String[] args) {
+	char[] name = {'a','j','a','i','s','h','a','r','m','a'};
+	int arrayLength = name.length;
+	for(int iterator = arrayLength - 1; i > arrayLength - 6; i--) {
+	    System.out.println(name[iterator]);
+	}
+    }
+}	
+	
