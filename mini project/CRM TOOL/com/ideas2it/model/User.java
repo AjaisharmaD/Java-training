ba2222745f97a256a17f55a15c2ee97969a5787f
Ajaisharma <ajaisharma187@gmail.com>
2022-09-16 17:53:13 +0530

    Update User.java

--- mini project/CRM TOOL/com/ideas2it/model/User.java
+++ mini project/CRM TOOL/com/ideas2it/model/User.java
@@ -7,16 +7,21 @@ package com.ideas2it.model;
  * @version 1.0  07-09-2022
  *
  */   
-public class User {  // name conven
+public class User { 
+    private String id;
     private String name;
     private String phoneNumber;
     private String emailId;
-    //private String passsword;
+    private String password;
 
     public User(String name, String emailId, String phoneNumber) {
         this.name = name;
         this.emailId = emailId;
         this.phoneNumber = phoneNumber;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public void setName(String name) {
@@ -30,11 +35,15 @@ public class User {  // name conven
     public void setEmailId(String emailId) {
         this.emailId = emailId;
     }
-    /*
+    
     public void setPassword(String password) {
         this.password = password;
     }
-    */
+
+    public String getId() {
+        return id;
+    }
+    
     public String getName() {
         return name;
     }
@@ -46,15 +55,16 @@ public class User {  // name conven
     public String getEmailId() {
         return emailId;
     }
-    /*
+    
     public String getPassword() {
         return password;
     }
-     */
+     
     @Override
     public String toString() {
          StringBuilder commonResponder = new StringBuilder();
-         commonResponder.append("\nName          : ").append(name)
+         commonResponder.append("\nID            : ").append(id)
+                        .append("\nName          : ").append(name)
                         .append("\nEmail ID      : ").append(emailId) 
                         .append("\nPhone Number  : ").append(phoneNumber);
          return commonResponder.toString();
