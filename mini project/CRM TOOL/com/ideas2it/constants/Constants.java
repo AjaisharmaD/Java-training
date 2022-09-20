43e608f38309079108515e11b5a2e13fb07edc4b
Ajaisharma <ajaisharma187@gmail.com>
2022-09-21 00:09:12 +0530

    Add files via upload

---
+++ mini project/CRM TOOL/com/ideas2it/constants/Constants.java
@@ -0,0 +1,35 @@
+package com.ideas2it.constants;
+
+/**
+ * Constants of the CRMTool will be in here
+ * 
+ * @author AJAISHARMA
+ * @version 1.0
+ * @since 19-09-2022
+ */
+public class Constants {
+    public final static byte EMPLOYEE = 1;
+    public final static byte MANAGER = 2;
+    public final static byte CRM_EXIT = 3;
+
+    public final static byte ADDER = 1;
+    public final static byte PROJECTOR = 2;
+    public final static byte FINDER = 3;
+    public final static byte UPDATER = 4;
+    public final static byte DELETOR = 5;
+    public final static byte EXIT = 6;
+
+    public final static byte NAME = 1;
+    public final static byte EMAIL = 2;
+    public final static byte PHONE_NUMBER = 3;
+    public final static byte STAGE = 4;
+    public final static byte COMPANY_NAME = 5;
+    public final static byte START_DATE = 6;
+    public final static byte END_DATE = 7;
+    public final static byte DEAL_SIZE = 8;
+    public final static byte EXIT_EMPLOYEE_UPDATER = 9; 
+
+    public final static byte EXIT_MANAGER_UPDATER = 4;
+
+    public final static byte LOGOUT = 1;
+}
