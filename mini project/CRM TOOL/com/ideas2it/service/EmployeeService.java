6ed41088a9d73903690724bd334e1cc2b5f227a0
Ajaisharma <ajaisharma187@gmail.com>
2022-09-21 00:12:31 +0530

    Update EmployeeService.java

--- mini project/CRM TOOL/com/ideas2it/service/EmployeeService.java
+++ mini project/CRM TOOL/com/ideas2it/service/EmployeeService.java
@@ -1,16 +1,27 @@
 package com.ideas2it.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import com.ideas2it.controller.EmployeeController;
-import com.ideas2it.dao.EmployeeDaoImpl;
+import com.ideas2it.dao.EmployeeDao;
+import com.ideas2it.dao.impl.EmployeeDaoImpl;
 import com.ideas2it.model.Lead;
 
+/**
+ * Stores and fetch the data from the Collection Storage
+ *
+ * @author AJAISHARMA
+ * @version 1.0
+ * @since 16-09-2022
+ */
 public class EmployeeService {
-    private EmployeeDaoImpl employeeDaoImpl;
+    private static int idCount = 0;
+    private EmployeeDao employeeDao;
     
     public EmployeeService() {
-        this.employeeDaoImpl = new EmployeeDaoImpl(); 
+        this.employeeDao = new EmployeeDaoImpl(); 
     }
    
     /**
@@ -18,39 +29,39 @@ public class EmployeeService {
      * 
      * @return returns the generated Id
      */
-    public String generateId() {
+    private String generateId() {   
         String prefixId = "Lead_0";
-        return prefixId + (++idCount);
+        return prefixId + (++idCount);  
     }
     
     /**
      * Adds the Lead's detail 
      *
      * @param lead - lead Object to add 
+     *
      * @return returns boolean
      */
     public boolean addLead(Lead lead) {
        String id = generateId();
-       return employeeDaoImpl.addLead(id, lead);
+       return employeeDao.addLead(id, lead);
     }
 
     /**   
-     * Prints all the lead's details  
-     *
-     * @return returns nothing
+     * Gets all the lead's details  
      */
-    public List<Lead> printLeads() {
-       return employeeDaoImpl.printLeads();
+    public List<Lead> getLeads() {     
+        return employeeDao.getLeads();
     }
 
     /**
-     * Prints the lead's Details by Id
+     * Gets the lead's Details by Id
      * 
      * @param id - Lead's Id to search the lead
+     *
      * @return one lead's Details
      */
-    public Lead printLeadById(String id) {
-       return employeeDaoImpl.printLeadById(id);
+    public Lead getLeadById(String id) {
+        return employeeDao.getLeadById(id);
     }
 
     /**
@@ -58,66 +69,72 @@ public class EmployeeService {
      *
      * @param id - key to update the name
      * @param leadName - updated name
+     *
      * @return boolean
      */
-     public boolean editName(String id, String leadName) {
-       return employeeDaoImpl.editName(id, leadName);
-     }
+    public boolean updateName(String id, String leadName) {
+        return employeeDao.updateName(id, leadName);
+    }
 
     /**
      * Updates the Email id of the Lead
      *
      * @param id - key to update the Email id
      * @param leadEmail - updated mail id
+     *
      * @return boolean
      */
-     public boolean editEmail(String id, String leadEmail) {
-       return employeeDaoImpl.editEmail(id, leadEmail);
-     }
+    public boolean updateEmail(String id, String leadEmail) {
+        return employeeDao.updateEmail(id, leadEmail);
+    }
 
     /**
      * Updates the Phone Number of the Lead
      *
      * @param id - key to update the Phone Number
      * @param leadPhone - updated Phone Number
+     *
      * @return boolean
      */
-     public boolean editPhone(String id, String leadPhone) {
-       return employeeDaoImpl.editPhone(id, leadPhone);
-     }
+    public boolean updatePhoneNumber(String id, String leadPhone) {
+        return employeeDao.updatePhoneNumber(id, leadPhone);
+    }
 
     /**
      * Updates the Stage of the Lead
      *
      * @param id - key to update the Stage
      * @param leadStage - updated Stage
+     *
      * @return boolean
      */
-     public boolean editStage(String id, String leadStage) {
-       return employeeDaoImpl.editEmail(id, leadStage);
-     }
+    public boolean updateStage(String id, String leadStage) {
+        return employeeDao.updateStage(id, leadStage);
+    }
 
     /**
      * Updates the Company Name of the Lead
      *
      * @param id - key to update the Company Name
      * @param leadCompanyName - updated Company Name
+     *
      * @return boolean
      */
-     public boolean editCompanyName(String id, String leadCompanyName) {
-       return employeeDaoImpl.editEmail(id, leadCompanyName);
-     }
+    public boolean updateCompanyName(String id, String leadCompanyName) {
+        return employeeDao.updateCompanyName(id, leadCompanyName);
+    }
 
     /**
      * Updates the Start Date of the Lead
      *
      * @param id - key to update the Start Date
      * @param leadStartDate - updated Start Date
+     *
      * @return boolean
      */
-     public boolean editStartDate(String id, String leadStartDate) {
-       return employeeDaoImpl.editEmail(id, leadStartDate);
-     }
+    public boolean updateStartDate(String id, String leadStartDate) {
+        return employeeDao.updateStartDate(id, leadStartDate);
+    }
 
 
     /**
@@ -125,30 +142,33 @@ public class EmployeeService {
      *
      * @param id - key to update the End Date
      * @param leadEndDate - updated End Date
+     *
      * @return boolean
      */
-     public boolean editEndDate(String id, String leadEndDate) {
-       return employeeDaoImpl.editEmail(id, leadEndDate);
-     }
+    public boolean updateEndDate(String id, String leadEndDate) {
+        return employeeDao.updateEndDate(id, leadEndDate);
+    }
 
     /**
      * Updates the Deal Size of the Lead
      *
      * @param id - key to update the Deal Size
      * @param leadDealSize - updated Deal Size
+     *
      * @return boolean
      */
-     public boolean editDealSize(String id, int leadDealSize) {
-       return employeeDaoImpl.editDealSize(id, leadDealSize);
-     }   
+    public boolean updateDealSize(String id, int leadDealSize) {
+        return employeeDao.updateDealSize(id, leadDealSize);
+    }   
 
     /**
      * Deletes the Lead by id
      *
      * @param id- key to delete the Lead
+     *
      * @return boolean
      */
     public boolean deleteLeadById(String id) {
-        return employeeDaoImpl.deleteLeadById(id);
+        return employeeDao.deleteLeadById(id);
     }
 }
