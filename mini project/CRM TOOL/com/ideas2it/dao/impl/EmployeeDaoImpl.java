777a3d7ce8b548f177fc78c0d8c94c8701b93400
Ajaisharma <ajaisharma187@gmail.com>
2022-09-21 21:37:31 +0530

    Update EmployeeDaoImpl.java

--- mini project/CRM TOOL/com/ideas2it/dao/impl/EmployeeDaoImpl.java
+++ mini project/CRM TOOL/com/ideas2it/dao/impl/EmployeeDaoImpl.java
@@ -18,189 +18,49 @@ import com.ideas2it.model.Lead;
  * @since 19-09-2022
  */
 public class EmployeeDaoImpl implements EmployeeDao {
-    private int idCount = 0;
     private Map<String, Lead> leadMap;
     
     public EmployeeDaoImpl() {
         this.leadMap = new HashMap<>(); 
     }
 
-    /** 
-     * {inheritdoc}
-     */
-    public boolean addLead(String leadId, Lead lead) {
+    @Override
+    public Lead addLead(String leadId, Lead lead) {
         lead.setId(leadId);
-        leadMap.put(leadId, lead);
-
-        if (leadMap.containsKey(leadId)) {
-            return true;
-        }
-        return false;
+        return leadMap.put(leadId, lead);
     }
 
-    /**   
-     * Prints all the lead's details  
-     *
-     * @return returns nothing
-     */
+    @Override
     public Map<String, Lead> getLeads() {
         return leadMap;
     }
 
-    /**
-     * Prints the lead's Details by Id
-     * 
-     * @param id - Lead's Id to search the lead
-     *
-     * @return one lead's Details
-     */
+    @Override
     public Lead getLeadById(String id) {
-        Lead lead = null;
+        Lead leads = null;
 
         if (leadMap.containsKey(id)) {
-            lead = leadMap.get(id);
+            leads = leadMap.get(id);
         }
-        return lead;
+        return leads;
     }
 
-    /**
-     * Updates the Name of the Lead
-     *
-     * @param id - key to update the name
-     * @param leadName - updated name
-     *
-     * @return boolean
-     */
-     public boolean updateName(String id, String leadName) {
-         if (leadMap.containsKey(id)) {
-             leadMap.get(id).setName(leadName);
-             return true;
-         }
-         return false;
-     }
+    @Override
+    public Lead updateLead(String id, Lead lead) {
+        Lead updatedLead = null;
 
-    /**
-     * Updates the Email id of the Lead
-     *
-     * @param id - key to update the Email id
-     * @param leadEmail - updated mail id
-     *
-     * @return boolean
-     */
-     public boolean updateEmail(String id, String leadEmail) {
-         if (leadMap.containsKey(id)) {
-             leadMap.get(id).setEmailId(leadEmail);
-             return true;
-         }
-         return false;
-     }
-
-    /**
-     * Updates the Phone Number of the Lead
-     *
-     * @param id - key to update the Phone Number
-     * @param leadPhone - updated Phone Number
-     *
-     * @return boolean
-     */
-     public boolean updatePhoneNumber(String id, String leadPhone) {
-         if (leadMap.containsKey(id)) {
-             leadMap.get(id).setPhoneNumber(leadPhone);
-             return true;
-         }
-         return false;
-     }
-
-    /**
-     * Updates the Stage of the Lead
-     *
-     * @param id - key to update the Stage
-     * @param leadStage - updated Stage
-     *
-     * @return boolean
-     */
-     public boolean updateStage(String id, String leadStage) {
-         if (leadMap.containsKey(id)) {
-             leadMap.get(id).setStage(leadStage);
-             return true;
-         }
-         return false;
-     }
-
-    /**
-     * Updates the Company Name of the Lead
-     *
-     * @param id - key to update the Company Name
-     * @param leadCompanyName - updated Company Name
-     *
-     * @return boolean
-     */
-    public boolean updateCompanyName(String id, String leadCompanyName) {
         if (leadMap.containsKey(id)) {
-            leadMap.get(id).setCompanyName(leadCompanyName);
-            return true;
+            updatedLead = leadMap.replace(id, lead);
         }
-        return false;
+        return updatedLead;
     }
 
-    /**
-     * Updates the Start Date of the Lead
-     *
-     * @param id - key to update the Start Date
-     * @param leadStartDate - updated Start Date
-     *
-     * @return boolean
-     */
-     public boolean updateStartDate(String id, String leadStartDate) {
-         if (leadMap.containsKey(id)) {
-             leadMap.get(id).setStartDate(leadStartDate);
-             return true;
-         }
-         return false;
-     }
-
-
-    /**
-     * Updates the End Date of the Lead
-     *
-     * @param id - key to update the End Date
-     * @param leadEndDate - updated End Date
-     *
-     * @return boolean
-     */
-     public boolean updateEndDate(String id, String leadEndDate) {
-         if (leadMap.containsKey(id)) {
-             leadMap.get(id).setEndDate(leadEndDate);
-             return true;
-         }
-         return false;
-     }
-
-    /**
-     * Updates the Deal Size of the Lead
-     *
-     * @param id - key to update the Deal Size
-     * @param leadDealSize - updated Deal Size
-     *
-     * @return boolean
-     */
-     public boolean updateDealSize(String id, int leadDealSize) {
-         if (leadMap.containsKey(id)) {
-             leadMap.get(id).setDealSize(leadDealSize);
-             return true;
-         }
-         return false;
-     }      
-
-    /**
-     * Deletes the Lead by Id
-     *
-     * @param id - key to Delete the Lead
-     *
-     * @return boolean
-     */
-    public boolean deleteLeadById(String id) {
-        leadMap.remove(id);
-        return true;
+    @Override
+    public Lead deleteLeadById(String id) {
+        Lead deletedLead = null;
+        if (leadMap.containsKey(id)) {
+            deletedLead =leadMap.remove(id);
+        }
+        return deletedLead;
     }
 }
