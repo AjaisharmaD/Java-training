d5f9079c7a895098583230484e67d4cf191814b7
Ajaisharma <ajaisharma187@gmail.com>
2022-09-22 19:58:39 +0530

    Update EmployeeController.java

--- mini project/CRM TOOL/com/ideas2it/controller/EmployeeController.java
+++ mini project/CRM TOOL/com/ideas2it/controller/EmployeeController.java
@@ -6,8 +6,12 @@ import com.ideas2it.model.Lead;
 import com.ideas2it.service.EmployeeService;
 
 /**
- * Controls all the operations performed by the employee
- * Like Adding, Printing, Searching, Updating the leads
+ * <h1> Employee Controller </h1>
+ * <p>
+ * This class will get request and return the responces
+ * like Adding, Updating, Viewing, Searching, Deleting
+ * the details of Leads
+ * </p> 
  *
  * @author Ajaisharma D
  * @version 1.0
@@ -22,54 +26,69 @@ public class EmployeeController {
     }
        
     /**
-     * Adds the Lead's details 
+     * <h1> Add Lead </h1>
+     * <p>
+     * Adds the Details of Leads 
+     * </p>
      *
-     * @param lead - lead Object to add 
+     * @param lead     - lead Object to add 
      *
-     * @return true if the Lead is added into map
+     * @return boolean - true if the Details of an Lead added otherwise false
      */
     public boolean isLeadAdded(Lead lead) {
         return employeeService.isLeadAdded(lead);
     }
 
     /**   
-     * Gets all the lead's details as List  
+     * <h1> Get Details of Leads </h1>
+     * <p>
+     * Gets the Employee Leads
+     * </p>
      *
-     * @return returns List of Details
+     * @return List - Details of Leads
      */
     public List<Lead> getLeads() {
         return employeeService.getLeads();
     }    
 
     /**
-     * Gets the lead's Details by Id
+     * <h1> Get Details of Lead by Id </h1>
+     * <p>
+     * Gets the Details of a Single Lead by Id
+     * </p>
      * 
-     * @param id - Lead's Id to search the lead
+     * @param id    - Lead's Id to search the lead
      *
-     * @return Lead object which contain the details of the Lead
+     * @return Lead - Details of a Single Lead
      */
     public Lead getLeadById(String id) {
         return employeeService.getLeadById(id);
     }
 
     /**
-     * Updates the Lead
+     * <h1> Update Details of Lead </h1>
+     * <p>
+     * Updates the Details of a Single Lead
+     * </p>
      *
-     * @param id - key to update the Lead
-     * @param lead - updated Lead 
+     * @param id       - key to update the Lead
+     * @param lead     - updated Lead 
      *
-     * @return true if the lead is Updated
+     * @return boolean - true if the Details of Lead are updated otherwise false
      */
     public boolean isLeadUpdated(String id, Lead lead) {
         return employeeService.isLeadUpdated(id, lead);
     }
 
     /**
-     * Deletes the Lead by id
+     * <h1> Delete Details of Lead by Id</h1>
+     * <p>
+     * Deletes the Details of a Single Lead
+     * </p>
      *
-     * @param id- key to delete the Lead
+     * @param id       - key to delete the Lead
      *
-     * @return true if the lead is deleted
+     * @return boolean - true if the Details of Lead are Deleted otherwise false
      */
     public boolean isLeadDeletedById(String id) {
         return employeeService.isLeadDeletedById(id);
