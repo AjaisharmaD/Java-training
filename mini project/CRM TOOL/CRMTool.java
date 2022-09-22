c3c69e247880aa7cf4003d54791f574dba870fe0
Ajaisharma <ajaisharma187@gmail.com>
2022-09-22 19:57:56 +0530

    Update CRMTool.java

--- mini project/CRM TOOL/CRMTool.java
+++ mini project/CRM TOOL/CRMTool.java
@@ -1,12 +1,24 @@
 import com.ideas2it.view.CRMView;
 
 /**
- * Start of the CRM Tool 
+ * <h1> CRM Tool <h1>
+ * <p>
+ * Start the CRM Tool 
+ * </p>
  *
- * @author Ajaisharma D
- * @version 1.0 12-09-2022
+ * @author  AJAISHARMA
+ * @version 1.0 
+ * @since   24-08-2022
  */
 public class CRMTool {
+    /**
+     * <h1> Main Method <h1>
+     * <p>
+     * This method will call the CRM Tool's Dashboard to start.
+     * </p>
+     *
+     * @param args - Unused.
+     */
     public static void main(String[] args) {
 	CRMView crmView = new CRMView();
         crmView.startCRM();
