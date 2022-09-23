0240e1773af023cf09b207270dbf099ba52e8e7b
Ajaisharma <ajaisharma187@gmail.com>
2022-09-23 22:53:42 +0530

    Update ManagerController.java

--- mini project/CRM TOOL/com/ideas2it/controller/ManagerController.java
+++ mini project/CRM TOOL/com/ideas2it/controller/ManagerController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.ideas2it.model.User;
 import com.ideas2it.service.ManagerService;
+import com.ideas2it.utils.ValidationUtils;
 
 /**
  * <h1> Manager Controller </h1>
@@ -19,9 +20,11 @@ import com.ideas2it.service.ManagerService;
  */
 public class ManagerController {
     private ManagerService managerService;
+    private ValidationUtils validationUtils;
     
     public ManagerController() {
         this.managerService = new ManagerService();
+        this.validationUtils = new ValidationUtils();
     }
 
     /**
@@ -78,5 +81,83 @@ public class ManagerController {
      */
     public boolean isEmployeeUpdated(String id, User user) {
         return managerService.isEmployeeUpdated(id, user);
+    }
+
+    /**
+     * <h1> Remove Details of Employee by Id</h1>
+     * <p>
+     * Removes the Details of a Single Employee
+     * </p>
+     *
+     * @param id       - key to remove the Employee
+     *
+     * @return boolean - true if the Details of Employee are Removed otherwise false
+     */
+    public boolean isEmployeeRemovedById(String id) {
+        return managerService.isEmployeeRemovedById(id);
+    }
+
+    /**
+     * <h1> Valid Name </h1>
+     * <p>
+     * This method will get the Name and checks whether the given Name is valid or not
+     * </p>
+     *
+     * @param name     - Name of Employee  
+     * @return boolean - true if the Name is valid otherwise false
+     */
+    public boolean isValidName(String name) {
+        if (validationUtils.isValidName(name)) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * <h1> Valid Email </h1>
+     * <p>
+     * This method will get the Email and checks whether the given Email is valid or not
+     * </p>
+     *
+     * @param email    - Email of Employee 
+     * @return boolean - true if the Email is valid otherwise false
+     */
+    public boolean isValidEmail(String email) {
+        if (validationUtils.isValidEmail(email)) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * <h1> Valid Phone Number </h1>
+     * <p>
+     * This method will get the Phone Number and checks whether the given Phone Number is valid or not
+     * </p>
+     *
+     * @param phoneNumber - Phone Number of Employee  
+     * @return boolean    - true if the Phone Number is valid otherwise false
+     */
+    public boolean isValidPhoneNumber(String phoneNumber) {
+        if (validationUtils.isValidPhoneNumber(phoneNumber)) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * <h1> Valid Password </h1>
+     * <p>
+     * This method will get the Password and checks whether the given Password is valid or not
+     * </p>
+     *
+     * @param password - Company Name of Employee  
+     * @return boolean - true if the Company Name is valid otherwise false
+     */
+    public boolean isValidPassword(String password) {
+        if (validationUtils.isValidPassword(password)) {
+            return true;
+        }
+        return false;
     }
 }
